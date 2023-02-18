5569f804708bc92825d28152ad693dc81ed45019
fazl1ddin <98404501+fazl1ddin@users.noreply.github.com>
2023-02-18 17:39:39 +0500

    new

--- index.cjs
+++ index.cjs
@@ -1,85 +1,236 @@
-// const sharp = require('sharp')
+const puppeteer = require('puppeteer')
+const fs = require('fs').promises
+const Jimp = require('jimp')
+const pixelmatch = require('pixelmatch')
+const { cv } = require('opencv-wasm')
 
-// sharp('img.png')
-//     .raw()
-//     .toBuffer((err, data, info) => {
-//         if(err) throw err
-//         else {
-//             const pixels = []
-//             const {width, height, channels} = info
-//             let targetX, targetY;
-//             const buffer = Buffer.alloc(width * height * channels)
-//             for (let index = 0; index < width * height * channels; index += channels) {
-//                     pixels.push({
-//                         r: data[index],
-//                         g: data[index + 1],
-//                         b: data[index + 2],
-//                         alpha: channels == 4 ? data[index + 3] : 255
-//                     });
-//                     buffer[index] = data[index]
-//                     buffer[index + 1] = data[index + 1]
-//                     buffer[index + 2] = data[index + 2]
-//                     buffer[index + 3] = channels == 4 ? data[index + 3] : 255
-//             }
-//             const coord = pixels.find((item) => item.r == 1 && item.g == 28 && item.b == 14)
-//             sharp(buffer, {raw: {width, height, channels}})
-//             .scan(0, 0, width, height, (x, y, idx) => {
-//                 const r = data[idx];
-//                 const g = data[idx + 1];
-//                 const b = data[idx + 2];
-      
-//                 // Вычисляем расстояние между искомым цветом и текущим пикселем
-//                 const currentDistance = Math.sqrt(
-//                   (targetColor[0] - r) ** 2 + (targetColor[1] - g) ** 2 + (targetColor[2] - b) ** 2
-//                 );
-      
-//                 // Если расстояние меньше, чем текущее минимальное расстояние,
-//                 // обновляем координаты и минимальное расстояние
-//                 if (currentDistance < distance) {
-//                   targetX = x;
-//                   targetY = y;
-//                   distance = currentDistance;
-//                 }
-//               })
-//               .then(() => {
-//                 console.log(`Ближайший пиксель к искомому цвету (${targetColor}) находится в точке (${targetX},${targetY})`);
-//               })
-//               .catch((err) => {
-//                 console.error(err);
-//               });
-//         }
-//     })
+async function findPuzzlePosition (page) {
+    let images = await page.$$eval('.geetest_canvas_img canvas', canvases => canvases.map(canvas => canvas.toDataURL().replace(/^data:image\/png;base64,/, '')))
 
-// sharp('img.png')
-//   .toBuffer()
-//   .then((data) => {
-//     const all = []
-//     const pixels = sharp(data)
-//       .metadata()
-//       .then((metadata) => {
-//         return sharp(data)
-//           .raw()
-//           .toBuffer({ resolveWithObject: true }) // Добавлено использование resolveWithObject
-//           .then(({ data, info }) => {
-//             const { width, height } = info;
-//             for (let y = 0; y < height; y++) {
-//               for (let x = 0; x < width; x++) {
-//                 const index = (y * width + x) * 3;
-//                 const r = data[index];
-//                 const g = data[index + 1];
-//                 const b = data[index + 2];
-//                 all.push({x, y, r, g, b})
-//                 // console.log(`Pixel at (${x}, ${y}): RGB(${r}, ${g}, ${b})`);
-//               }
-//             }
-//             console.log(all.find((item) => {
-//                 if(item.r == 3 && item.g == 45 && item.b == 22) return true
-//                 return false
-//             }))
-//           });
-//       });
-//     return pixels;
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+    await fs.writeFile(`./puzzle.png`, images[1], 'base64')
+
+    let srcPuzzleImage = await Jimp.read('./puzzle.png')
+    let srcPuzzle = cv.matFromImageData(srcPuzzleImage.bitmap)
+    let dstPuzzle = new cv.Mat()
+
+    cv.cvtColor(srcPuzzle, srcPuzzle, cv.COLOR_BGR2GRAY)
+    cv.threshold(srcPuzzle, dstPuzzle, 127, 255, cv.THRESH_BINARY)
+
+    let kernel = cv.Mat.ones(5, 5, cv.CV_8UC1)
+    let anchor = new cv.Point(-1, -1)
+    cv.dilate(dstPuzzle, dstPuzzle, kernel, anchor, 1)
+    cv.erode(dstPuzzle, dstPuzzle, kernel, anchor, 1)
+
+    let contours = new cv.MatVector()
+    let hierarchy = new cv.Mat()
+    cv.findContours(dstPuzzle, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    let contour = contours.get(0)
+    let moment = cv.moments(contour)
+
+    return [Math.floor(moment.m10 / moment.m00), Math.floor(moment.m01 / moment.m00)]
+}
+
+async function findDiffPosition (page) {
+    await page.waitFor(100)
+
+    let srcImage = await Jimp.read('./diff.png')
+    let src = cv.matFromImageData(srcImage.bitmap)
+
+    let dst = new cv.Mat()
+    let kernel = cv.Mat.ones(5, 5, cv.CV_8UC1)
+    let anchor = new cv.Point(-1, -1)
+
+    cv.threshold(src, dst, 127, 255, cv.THRESH_BINARY)
+    cv.erode(dst, dst, kernel, anchor, 1)
+    cv.dilate(dst, dst, kernel, anchor, 1)
+    cv.erode(dst, dst, kernel, anchor, 1)
+    cv.dilate(dst, dst, kernel, anchor, 1)
+
+    cv.cvtColor(dst, dst, cv.COLOR_BGR2GRAY)
+    cv.threshold(dst, dst, 150, 255, cv.THRESH_BINARY_INV)
+
+    let contours = new cv.MatVector()
+    let hierarchy = new cv.Mat()
+    cv.findContours(dst, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    let contour = contours.get(0)
+    let moment = cv.moments(contour)
+
+    return [Math.floor(moment.m10 / moment.m00), Math.floor(moment.m01 / moment.m00)]
+}
+
+async function saveSliderCaptchaImages(page) {
+    await page.waitForSelector('.tab-item.tab-item-1')
+    await page.click('.tab-item.tab-item-1')
+
+    await page.waitForSelector('[aria-label="Click to verify"]')
+    await page.waitFor(1000)
+
+    await page.click('[aria-label="Click to verify"]')
+
+    await page.waitForSelector('.geetest_canvas_img canvas', { visible: true })
+    await page.waitFor(1000)
+    let images = await page.$$eval('.geetest_canvas_img canvas', canvases => {
+        return canvases.map(canvas => canvas.toDataURL().replace(/^data:image\/png;base64,/, ''))
+    })
+
+    await fs.writeFile(`./captcha.png`, images[0], 'base64')
+    await fs.writeFile(`./original.png`, images[2], 'base64')
+}
+
+async function saveDiffImage() {
+    const originalImage = await Jimp.read('./original.png')
+    const captchaImage = await Jimp.read('./captcha.png')
+
+    const { width, height } = originalImage.bitmap
+    const diffImage = new Jimp(width, height)
+
+    const diffOptions = { includeAA: true, threshold: 0.2 }
+
+    pixelmatch(originalImage.bitmap.data, captchaImage.bitmap.data, diffImage.bitmap.data, width, height, diffOptions)
+    diffImage.write('./diff.png')
+}
+
+async function run () {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: { width: 1366, height: 768 }
+    })
+    const page = await browser.newPage()
+
+    await page.goto('https://www.geetest.com/en/demo', { waitUntil: 'networkidle2' })
+
+    await page.waitFor(1000)
+
+    await saveSliderCaptchaImages(page)
+    await saveDiffImage()
+
+    let [cx, cy] = await findDiffPosition(page)
+
+    const sliderHandle = await page.$('.geetest_slider_button')
+    const handle = await sliderHandle.boundingBox()
+
+    let xPosition = handle.x + handle.width / 2
+    let yPosition = handle.y + handle.height / 2
+    await page.mouse.move(xPosition, yPosition)
+    await page.mouse.down()
+
+    xPosition = handle.x + cx - handle.width / 2
+    yPosition = handle.y + handle.height / 3
+    await page.mouse.move(xPosition, yPosition, { steps: 25 })
+
+    await page.waitFor(100)
+
+    let [cxPuzzle, cyPuzzle] = await findPuzzlePosition(page)
+
+    xPosition = xPosition + cx - cxPuzzle
+    yPosition = handle.y + handle.height / 2
+    await page.mouse.move(xPosition, yPosition, { steps: 5 })
+    await page.mouse.up()
+
+    await page.waitFor(3000)
+    // success!
+
+
+    await browser.close()
+}
+
+// run()
+
+(async function() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: { width: 1366, height: 768 }
+    });
+    const page = await browser.newPage();
+    await page.goto('https://app.stormgain.com#modal_login');
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    await page.waitForSelector('#email');
+    await page.evaluate(() => {
+        const emailInput = document.querySelector('#email');
+        emailInput.value = 'fasasda@gfdgdf.com';
+    });
+
+    await page.waitForSelector('#password');
+    await page.evaluate(() => {
+        const passwordInput = document.querySelector('#password');
+        passwordInput.value = 'afafafdfs';
+    });
+
+    const buttonLogin = await page.waitForSelector('.controls input')
+    await buttonLogin.click()
+
+    await page.waitForSelector('.geetest_canvas_img canvas', { visible: true })
+    await page.waitFor(1000)
+    let images = await page.$$eval('.geetest_canvas_img canvas', canvases => {
+        return canvases.map(canvas => canvas.toDataURL().replace(/^data:image\/png;base64,/, ''))
+    })
+
+    await fs.writeFile(`./captcha.png`, images[0], 'base64')
+    await fs.writeFile(`./original.png`, images[2], 'base64')
+
+    const originalImage = await Jimp.read('./original.png')
+    const captchaImage = await Jimp.read('./captcha.png')
+
+    const { width, height } = originalImage.bitmap
+    const diffImage = new Jimp(width, height)
+
+    const diffOptions = { includeAA: true, threshold: 0.2 }
+
+    pixelmatch(originalImage.bitmap.data, captchaImage.bitmap.data, diffImage.bitmap.data, width, height, diffOptions)
+    diffImage.write('./diff.png')
+    
+    await page.waitFor(100)
+
+    let srcImage = await Jimp.read('./diff.png')
+    let src = cv.matFromImageData(srcImage.bitmap)
+
+    let dst = new cv.Mat()
+    let kernel = cv.Mat.ones(5, 5, cv.CV_8UC1)
+    let anchor = new cv.Point(-1, -1)
+
+    cv.threshold(src, dst, 127, 255, cv.THRESH_BINARY)
+    cv.erode(dst, dst, kernel, anchor, 1)
+    cv.dilate(dst, dst, kernel, anchor, 1)
+    cv.erode(dst, dst, kernel, anchor, 1)
+    cv.dilate(dst, dst, kernel, anchor, 1)
+
+    cv.cvtColor(dst, dst, cv.COLOR_BGR2GRAY)
+    cv.threshold(dst, dst, 150, 255, cv.THRESH_BINARY_INV)
+
+    let contours = new cv.MatVector()
+    let hierarchy = new cv.Mat()
+    cv.findContours(dst, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    let contour = contours.get(0)
+    let moment = cv.moments(contour)
+
+    const [cx, cy] =  [Math.floor(moment.m10 / moment.m00), Math.floor(moment.m01 / moment.m00)]
+
+    const sliderHandle = await page.$('.geetest_slider_button')
+    const handle = await sliderHandle.boundingBox()
+
+    let xPosition = handle.x + handle.width / 2
+    let yPosition = handle.y + handle.height / 2
+    await page.mouse.move(xPosition, yPosition)
+    await page.mouse.down()
+
+    xPosition = handle.x + cx - handle.width / 2
+    yPosition = handle.y + handle.height / 3
+    await page.mouse.move(xPosition, yPosition, { steps: 25 })
+
+    await page.waitFor(100)
+
+    let [cxPuzzle, cyPuzzle] = await findPuzzlePosition(page)
+
+    xPosition = xPosition + cx - cxPuzzle
+    yPosition = handle.y + handle.height / 2
+    await page.mouse.move(xPosition, yPosition, { steps: 5 })
+    await page.mouse.up()
+
+    await page.waitFor(3000)
+
+    await browser.close()
+})();
